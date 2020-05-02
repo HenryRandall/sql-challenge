a27c5844bee2a0a0d749559036d2521c7db0bd8f
unknown <henrywrandall@gmail.com>
2020-05-02 08:38:08 -0400

    finished the bonus and corrected issues with schema

--- Data_Engineering.sql
+++ Data_Engineering.sql
@@ -9,51 +9,53 @@ DROP TABLE IF EXISTS titles CASCADE;
 -- Create SQL tables to import CSVs into.
 -- Start with the 2 tables that the others have dependancies on
 CREATE TABLE "departments" (
-    "dept_no" VARCHAR(15)   NOT NULL PRIMARY KEY,
-    "dept_name" VARCHAR(30)  NOT NULL
+    "dept_no" VARCHAR(15) NOT NULL PRIMARY KEY,
+    "dept_name" VARCHAR(30) NOT NULL
 );
 
 CREATE TABLE "employees" (
-    "emp_no" INT   NOT NULL PRIMARY KEY,
-    "birth_date" DATE   NOT NULL,
-    "first_name" VARCHAR(30)   NOT NULL,
-    "last_name" VARCHAR(30)   NOT NULL,
-    "gender" VARCHAR(3)   NOT NULL,
-    "hire_date" DATE   NOT NULL
+    "emp_no" INT NOT NULL PRIMARY KEY,
+    "birth_date" DATE NOT NULL,
+    "first_name" VARCHAR(30) NOT NULL,
+    "last_name" VARCHAR(30) NOT NULL,
+    "gender" VARCHAR(3) NOT NULL,
+    "hire_date" DATE NOT NULL
 );
 
 -- Create the tables with foregin keys to reference the first 2 tables
 CREATE TABLE "dept_emp" (
-    "emp_no" INT   NOT NULL,
-    "dept_no" VARCHAR(15)   NOT NULL,
-    "from_date" DATE   NOT NULL,
-    "to_date" DATE   NOT NULL,
+    "emp_no" INT NOT NULL,
+    "dept_no" VARCHAR(15) NOT NULL,
+    "from_date" DATE NOT NULL,
+    "to_date" DATE,
+	PRIMARY KEY (emp_no, dept_no),
 	FOREIGN KEY (emp_no) REFERENCES employees(emp_no),
 	FOREIGN KEY (dept_no) REFERENCES departments(dept_no)
 );
 
 CREATE TABLE "dept_manager" (
-    "dept_no" VARCHAR(15)  NOT NULL,
-    "emp_no" INT   NOT NULL,
-    "from_date" DATE   NOT NULL,
-    "to_date" DATE   NOT NULL,
+    "dept_no" VARCHAR(15) NOT NULL,
+    "emp_no" INT NOT NULL PRIMARY KEY,
+    "from_date" DATE NOT NULL,
+    "to_date" DATE,
 	FOREIGN KEY (emp_no) REFERENCES employees(emp_no),
 	FOREIGN KEY (dept_no) REFERENCES departments(dept_no)
 );
 
 CREATE TABLE "salaries" (
-    "emp_no" INT   NOT NULL,
-    "salary" MONEY   NOT NULL,
-    "from_date" DATE   NOT NULL,
-    "to_date" DATE   NOT NULL,
+    "emp_no" INT NOT NULL PRIMARY KEY,
+    "salary" MONEY NOT NULL,
+    "from_date" DATE NOT NULL,
+    "to_date" DATE,
 	FOREIGN KEY (emp_no) REFERENCES employees(emp_no)
 );
 
 CREATE TABLE "titles" (
-    "emp_no" INT   NOT NULL,
-    "title" VARCHAR(30)   NOT NULL,
-    "from_date" DATE   NOT NULL,
-    "to_date" DATE   NOT NULL,
+    "emp_no" INT NOT NULL,
+    "title" VARCHAR(30) NOT NULL,
+    "from_date" DATE NOT NULL,
+    "to_date" DATE,
+	PRIMARY KEY (emp_no, title, from_date),
 	FOREIGN KEY (emp_no) REFERENCES employees(emp_no)
 );
 
